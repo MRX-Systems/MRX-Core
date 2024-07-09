7dc29cf290d4c306e095855477b17b952aed0c72
ruby <necrelox@proton.me>
2024-07-09 10:51:23 +0200

    fix: correction build

--- Source/Common/Util/Hash.ts
+++ Source/Common/Util/Hash.ts
@@ -7,7 +7,7 @@ import { type Hash as TypeHash, createHash } from 'crypto';
  *
  * @returns The MD5 hash of the data.
  */
-export function md5(data: unknown): string {
+function md5(data: unknown): string {
     const str: string = typeof data === 'string' ? data : JSON.stringify(data);
     const hash: TypeHash = createHash('md5');
     return hash.update(str).digest('hex');
@@ -20,7 +20,7 @@ export function md5(data: unknown): string {
  *
  * @returns The SHA-256 hash of the data.
  */
-export function sha256(data: unknown): string {
+function sha256(data: unknown): string {
     const str: string = typeof data === 'string' ? data : JSON.stringify(data);
     const hash: TypeHash = createHash('sha256');
     return hash.update(str).digest('hex');
@@ -33,8 +33,14 @@ export function sha256(data: unknown): string {
  *
  * @returns The SHA-512 hash of the data.
  */
-export function sha512(data: unknown): string {
+function sha512(data: unknown): string {
     const str: string = typeof data === 'string' ? data : JSON.stringify(data);
     const hash: TypeHash = createHash('sha512');
     return hash.update(str).digest('hex');
 }
+
+export const Hash = {
+    md5,
+    sha256,
+    sha512,
+};
