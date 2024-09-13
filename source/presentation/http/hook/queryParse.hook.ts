405fe411e6b06d5ce466128ba7ac29d0a88ee5b1
ruby <necrelox@proton.me>
2024-09-13 11:18:03 +0200

    feat: add hook pase query for validation + remove keyinclusion

---
+++ source/presentation/http/hook/queryParse.hook.ts
@@ -0,0 +1,30 @@
+import type { FastifyInstance, FastifyReply, FastifyRequest, HookHandlerDoneFunction } from 'fastify';
+
+import type { Hook } from '#/common/types/index.js';
+
+/**
+ * Query Parse Hook class implement the Hook interface ({@link Hook})
+ *
+ * This hook is responsible for parsing the query string to JSON.
+ */
+export class QueryParseHook implements Hook {
+
+    /**
+     * Configure the hook
+     *
+     * @param app - The Fastify instance. ({@link FastifyInstance})
+     */
+    public configure(app: FastifyInstance): void {
+        app.addHook('preValidation', (req: FastifyRequest, _: FastifyReply, done: HookHandlerDoneFunction) => {
+            const query: Record<string, unknown> = req.query as Record<string, unknown>;
+            for (const key in query)
+                if (typeof query[key] === 'string' && query[key].startsWith('{'))
+                    try {
+                        query[key] = JSON.parse(query[key]);
+                    } catch {
+                        continue;
+                    }
+            done();
+        });
+    }
+}
