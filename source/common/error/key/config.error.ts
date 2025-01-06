742ae1fe49729df56ab7437e8d21ccb0307b1feb
ruby <necrelox@proton.me>
2025-01-06 11:49:22 +0100

    fix(🔧): add 'as const' assert to CONFIG_ERRORS and UTIL_ERRORS, immuable

--- source/common/error/key/config.error.ts
+++ source/common/error/key/config.error.ts
@@ -4,4 +4,4 @@
 export const CONFIG_ERRORS: Record<string, [string, number]> = {
     FORMAT_ALREADY_EXISTS: ['error.core.validation.format.already_exists', 500],
     FORMAT_DOES_NOT_EXIST: ['error.core.validation.format.does_not_exist', 500]
-};
+} as const;
