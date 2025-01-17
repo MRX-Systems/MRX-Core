a870ff8c3352b859f3939376d0899f80df454328
ruby <necrelox@proton.me>
2025-01-17 09:24:47 +0100

    docs(📖):  Add detailed JSDoc/TSDoc comments to Table class

--- source/core/database/table.ts
+++ source/core/database/table.ts
@@ -1,14 +1,37 @@
 import EventEmitter from 'events';
 
+/**
+ * Represents a table in the database.
+ * Extends the {@link EventEmitter} class to support event emission.
+ */
 export class Table extends EventEmitter {
+    /**
+     * The name of the database.
+     */
     private readonly _databaseName: string;
 
+    /**
+     * The fields (columns) of the table.
+     */
     private readonly _fields: string[] = [];
 
+    /**
+     * The name of the table.
+     */
     private readonly _name: string;
 
+    /**
+     * The primary key of the table.
+     */
     private readonly _primaryKey: [string, 'NUMBER' | 'STRING'];
 
+    /**
+     * Creates a new instance of the `Table` class.
+     * @param databaseName - The name of the database.
+     * @param tableName - The name of the table.
+     * @param fields - The fields (columns) of the table.
+     * @param primaryKey - The primary key of the table.
+     */
     public constructor(
         databaseName: string,
         tableName: string,
@@ -22,18 +45,30 @@ export class Table extends EventEmitter {
         this._primaryKey = primaryKey;
     }
 
+    /**
+     * Gets the name of the database.
+     */
     public get databaseName(): string {
         return this._databaseName;
     }
 
+    /**
+     * Gets the name of the table.
+     */
     public get name(): string {
         return this._name;
     }
 
+    /**
+     * Gets the fields (columns) of the table.
+     */
     public get fields(): string[] {
         return this._fields;
     }
 
+    /**
+     * Gets the primary key of the table.
+     */
     public get primaryKey(): [string, 'NUMBER' | 'STRING'] {
         return this._primaryKey;
     }
