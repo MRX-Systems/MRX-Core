dd0eb052e7e79b7816393e7705c230ee7903c866
ruby <necrelox@proton.me>
2025-05-20 17:59:49 +0200

    feat(🚀): [Add SingletonManager and error handling enums]
    ## Features
    - Introduced `SingletonManager` for managing singleton instances.
    - Added `singletonManagerErrorKeys` for error handling related to class registration.
    
    ## Description
    This commit implements a `SingletonManager` class that allows for the registration and retrieval of singleton instances in the application. It includes error handling for cases where a class constructor is already registered or not registered at all, utilizing the `singletonManagerErrorKeys` for consistent error messaging.

---
+++ source/singletonManager/enums/singletonManagerErrorKeys.ts
@@ -0,0 +1,7 @@
+/**
+ * Core util key error is a list of errors in the util context.
+ */
+export const singletonManagerErrorKeys = {
+    classConstructorAlreadyRegistered: 'basalt-helper.error.class_constructor_already_registered',
+    classConstructorNotRegistered: 'basalt-helper.error.class_constructor_not_registered'
+} as const;
