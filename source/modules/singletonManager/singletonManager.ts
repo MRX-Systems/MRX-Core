15d7309f21ac933ddded4d55dd230ee5bf113976
ruby <necrelox@proton.me>
2025-06-16 12:23:02 +0200

    refactor(🧹): [Refactor SingletonManager class for clarity and structure] - Updated class name from SingletonManagerSingleton to SingletonManager. - Removed unnecessary singleton instance logic, making the class static. - Simplified method signatures and documentation for better clarity. - Enhanced comments to accurately reflect the class's functionality.

--- source/modules/singletonManager/singletonManager.ts
+++ source/modules/singletonManager/singletonManager.ts
@@ -2,8 +2,8 @@ import { CoreError } from '#/error/coreError';
 import { singletonManagerErrorKeys } from './enums/singletonManagerErrorKeys';
 
 /**
- * SingletonManagerSingleton is a singleton class that manages the singletons in the application.
- * When a class is registered, the SingletonManagerSingleton creates a new instance of the class when it is requested.
+ * SingletonManager is a static class that manages the singletons in the application.
+ * When a class is registered, the SingletonManager creates a new instance of the class when it is requested.
  *
  * @example
  * ```ts
@@ -28,31 +28,16 @@ import { singletonManagerErrorKeys } from './enums/singletonManagerErrorKeys';
  * SingletonManager.get<ExampleSingleton>('ExampleSingleton').sayHello(); // Output: Hello from instance 1!
  * ```
  */
-class SingletonManagerSingleton {
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
+export class SingletonManager {
     /**
-     * _instance is a private static property that holds the singleton instance of the class. ({@link SingletonManagerSingleton})
+     * _registry is a private static property that holds the class instances that are registered
+     * in the SingletonManager. The key is the name of the class and the value is the instance of the class.
      */
-    private static _instance: SingletonManagerSingleton;
+    private static readonly _registry = new Map<string, unknown>();
 
     /**
-     * _classConstructor is a private property that holds the class constructors that are registered
-     * in the SingletonManagerSingleton. The key is the name of the class and the value is the constructor of the class.
-     */
-    private readonly _registry = new Map<string, unknown>();
-
-    /**
-     * Constructor of the SingletonManagerSingleton class.
-     *
-     * @returns The singleton instance of the SingletonManagerSingleton class. ({@link SingletonManagerSingleton})
-     */
-    public static get instance(): SingletonManagerSingleton {
-        if (!this._instance)
-            this._instance = new SingletonManagerSingleton();
-        return this._instance;
-    }
-
-    /**
-     * Registers a class constructor in the SingletonManagerSingleton.
+     * Registers a class constructor in the SingletonManager.
      *
      * @template TClass - The type of the class.
      * @template TArgs - The tuple type of the constructor arguments.
@@ -61,9 +46,9 @@ class SingletonManagerSingleton {
      * @param constructor - The constructor of the class.
      * @param args - The arguments to pass to the constructor of the class.
      *
-     * @throws ({@link CoreError}) If the class constructor is already registered, it throws an error. ({@link singletonManagerErrorKeys.classConstructorAlreadyRegistered})
+     * @throws ({@link CoreError}) If the class constructor is already registered, it throws an error.
      */
-    public register<
+    public static register<
         TClass extends object,
         TArgs extends unknown[]
     >(
@@ -81,13 +66,13 @@ class SingletonManagerSingleton {
     }
 
     /**
-     * Unregisters a class from the SingletonManagerSingleton.
+     * Unregisters a class from the SingletonManager.
      *
      * @param name - The name of the class to unregister.
-     *
-     * @throws ({@link CoreError}) If the class constructor is not registered, it throws an error. ({@link singletonManagerErrorKeys.classConstructorNotRegistered})
+     *²
+     * @throws ({@link CoreError}) If the class constructor is not registered, it throws an error.
      */
-    public unregister(name: string): void {
+    public static unregister(name: string): void {
         if (!this._registry.has(name))
             throw new CoreError({
                 key: singletonManagerErrorKeys.classConstructorNotRegistered,
@@ -104,11 +89,11 @@ class SingletonManagerSingleton {
      *
      * @param name - The name of the class to get the singleton instance.
      *
-     * @throws ({@link CoreError}) If the class is not registered, it throws an error. ({@link singletonManagerErrorKeys.classConstructorNotRegistered})
+     * @throws ({@link CoreError}) If the class is not registered, it throws an error.
      *
-     * @returns The singleton instance of the class. ({@link TClass})
+     * @returns The singleton instance of the class.
      */
-    public get<TClass>(name: string): TClass {
+    public static get<TClass>(name: string): TClass {
         if (!this._registry.has(name))
             throw new CoreError({
                 key: singletonManagerErrorKeys.classConstructorNotRegistered,
@@ -119,42 +104,13 @@ class SingletonManagerSingleton {
     }
 
     /**
-     * Checks if the class is registered in the SingletonManagerSingleton.
+     * Checks if the class is registered in the SingletonManager.
      *
      * @param name - The name of the class to check if it is registered.
      *
      * @returns True if the class is registered, otherwise false.
      */
-    public has(name: string): boolean {
+    public static has(name: string): boolean {
         return this._registry.has(name);
     }
 }
-
-/**
- * SingletonManager is an instance of the SingletonManagerSingleton class that manages the singletons in the application.
- * When a class is registered, the SingletonManagerSingleton creates a new instance of the class when it is requested.
- *
- * @example
- * ```ts
- * class ExampleSingleton {
- *    private static _count = 0;
- *    private readonly _id: number;
- *
- *    public constructor() {
- *       ExampleSingleton._count += 1;
- *      this._id = ExampleSingleton._count;
- *      console.log(`ExampleSingleton created with ID: ${this._id}`);
- *    }
- *
- *    public sayHello(): void {
- *      console.log(`Hello from instance ${this._id}!`);
- *    }
- * }
- *
- * SingletonManager.register('ExampleSingleton', ExampleSingleton);
- *
- * SingletonManager.get<ExampleSingleton>('ExampleSingleton').sayHello(); // Output: ExampleSingleton created with ID: 1 /n Hello from instance 1!
- * SingletonManager.get<ExampleSingleton>('ExampleSingleton').sayHello(); // Output: Hello from instance 1!
- * ```
- */
-export const SingletonManager = SingletonManagerSingleton.instance;
