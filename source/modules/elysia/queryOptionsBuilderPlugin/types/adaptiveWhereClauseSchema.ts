33cc4836920989f82b61d256fe17d808885d6047
ruby <necrelox@proton.me>
2025-06-17 12:08:38 +0200

    types(🌊): [Refactor AdaptiveWhereClauseSchema type definitions] ## Refactoring - Replaced `TOptional` with `TValue` for required fields. - Simplified the structure of the `AdaptiveWhereClauseSchema` type. - Enhanced clarity by removing unnecessary optionality in type definitions.

--- source/modules/elysia/queryOptionsBuilderPlugin/types/adaptiveWhereClauseSchema.ts
+++ source/modules/elysia/queryOptionsBuilderPlugin/types/adaptiveWhereClauseSchema.ts
@@ -5,44 +5,46 @@ import type {
     TInteger,
     TNumber,
     TObject,
-    TOptional,
+    TPartial,
     TSchema,
     TString,
     TTuple
 } from '@sinclair/typebox';
 
 export type AdaptiveWhereClauseSchema<TValue extends TSchema> =
-    TValue extends TString
-        ? TObject<{
-            $eq: TOptional<TValue>;
-            $neq: TOptional<TValue>;
-            $isNull: TOptional<TBoolean>;
-        } & {
-            $in: TOptional<TArray<TValue>>;
-            $nin: TOptional<TArray<TValue>>;
-            $like: TOptional<TString>;
-            $nlike: TOptional<TString>;
-        }>
-        : TValue extends TNumber | TDate | TInteger
+    TPartial<(
+        TValue extends TString
             ? TObject<{
-                $eq: TOptional<TValue>;
-                $neq: TOptional<TValue>;
-                $isNull: TOptional<TBoolean>;
+                $eq: TValue;
+                $neq: TValue;
+                $isNull: TBoolean;
             } & {
-                $in: TOptional<TArray<TValue>>;
-                $nin: TOptional<TArray<TValue>>;
-                $like: TOptional<TString>;
-                $nlike: TOptional<TString>;
-            } & {
-                $lt: TOptional<TValue>;
-                $lte: TOptional<TValue>;
-                $gt: TOptional<TValue>;
-                $gte: TOptional<TValue>;
-                $between: TOptional<TTuple<[TValue, TValue]>>;
-                $nbetween: TOptional<TTuple<[TValue, TValue]>>;
+                $in: TArray<TValue>;
+                $nin: TArray<TValue>;
+                $like: TString;
+                $nlike: TString;
             }>
-            : TObject<{
-                $eq: TOptional<TValue>;
-                $neq: TOptional<TValue>;
-                $isNull: TOptional<TBoolean>;
-            }>;
+            : TValue extends TNumber | TDate | TInteger
+                ? TObject<{
+                    $eq: TValue;
+                    $neq: TValue;
+                    $isNull: TBoolean;
+                } & {
+                    $in: TArray<TValue>;
+                    $nin: TArray<TValue>;
+                    $like: TString;
+                    $nlike: TString;
+                } & {
+                    $lt: TValue;
+                    $lte: TValue;
+                    $gt: TValue;
+                    $gte: TValue;
+                    $between: TTuple<[TValue, TValue]>;
+                    $nbetween: TTuple<[TValue, TValue]>;
+                }>
+                : TObject<{
+                    $eq: TValue;
+                    $neq: TValue;
+                    $isNull: TBoolean;
+                }>
+    )>;
