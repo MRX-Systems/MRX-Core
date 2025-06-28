03053cfb6d88df4ebde905ce645bc6851ff227bf
ruby <necrelox@proton.me>
2025-06-28 13:07:42 +0200

    refactor(🧹): [Refactor createOrderBySchema for improved structure]
    ## Refactoring
    - Changed the return type of `createOrderBySchema` from `t.Tuple` to `t.Union`.
    - Updated the schema structure to include both object and array formats for selected fields and directions.
    
    ## Description
    This refactor enhances the `createOrderBySchema` function by allowing it to return a more flexible schema that can handle both single and multiple order by conditions. The description for the schema has also been improved for clarity.

--- source/modules/elysia/queryOptionsBuilderPlugin/utils/createOrderBySchema.ts
+++ source/modules/elysia/queryOptionsBuilderPlugin/utils/createOrderBySchema.ts
@@ -10,9 +10,17 @@ import { t } from 'elysia/type-system';
  *
  * @returns A tuple schema with field name and direction
  */
-export const createOrderBySchema = <TInferedObject extends TObject>(schema: TInferedObject) => t.Tuple([
-    t.Extract(t.KeyOf(schema), t.String()),
-    t.Union([t.Literal('asc'), t.Literal('desc')])
+export const createOrderBySchema = <TInferedObject extends TObject>(schema: TInferedObject) => t.Union([
+    t.Object({
+        selectedField: t.KeyOf(schema),
+        direction: t.Union([t.Literal('asc'), t.Literal('desc')])
+    }),
+    t.Array(t.Object({
+        selectedField: t.KeyOf(schema),
+        direction: t.Union([t.Literal('asc'), t.Literal('desc')])
+    }), {
+        minItems: 1
+    })
 ], {
-    description: 'Field to order by and direction. Use "asc" for ascending or "desc" for descending order.'
+    description: 'Order by is an item or array of items with a field to order by and direction. Use "asc" for ascending or "desc" for descending order.'
 });
