5c754ba1143f679b4a2a1721efdf6ea8a7b5d12b
ruby <necrelox@proton.me>
2025-07-14 16:11:24 +0200

    refactor(🧹): [Rename createOrderBySchema to createOrderSchema]
    ## Refactoring
    - Renamed `createOrderBySchema` to `createOrderSchema` for consistency.
    ## Description
    This change improves the clarity of the function name, aligning it with its purpose and enhancing code readability.

--- source/modules/elysia/queryOptionsBuilderPlugin/utils/index.ts
+++ source/modules/elysia/queryOptionsBuilderPlugin/utils/index.ts
@@ -1,5 +1,5 @@
 export { createAdaptiveWhereClauseSchema } from './createAdaptiveWhereClauseSchema';
-export { createOrderBySchema } from './createOrderBySchema';
+export { createOrderSchema } from './createOrderSchema';
 export { createQSchema } from './createQSchema';
 export { createSelectedFieldsSchema } from './createSelectedFieldsSchema';
 export { isDateFromElysiaTypeBox } from './isDateFromElysiaTypeBox';
