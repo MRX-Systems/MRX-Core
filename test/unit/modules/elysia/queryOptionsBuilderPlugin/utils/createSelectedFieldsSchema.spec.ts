a8bae0ac4766357e2e8feb3dc2eb9630ca884d38
ruby <necrelox@proton.me>
2025-07-15 09:19:25 +0200

    test(🧪): [Fix create selected fields schema tests]

--- test/unit/modules/elysia/queryOptionsBuilderPlugin/utils/createSelectedFieldsSchema.spec.ts
+++ test/unit/modules/elysia/queryOptionsBuilderPlugin/utils/createSelectedFieldsSchema.spec.ts
@@ -28,8 +28,7 @@ describe('createSelectedFieldsSchema', () => {
 		expect(selectedFieldsSchema.description).toBe('Selected fields can be a single field, a wildcard "*", or an array of fields and/or wildcard "*".');
 	});
 
-
-	test('should have a good first element (Keyof Schema)', () => {
+	test('should have a good first element - Keyof Schema', () => {
 		const selectedFieldsSchema = createSelectedFieldsSchema(baseSchema);
 		const [firstElement] = selectedFieldsSchema.anyOf;
 		expect(firstElement[Kind]).toBe('Union');
@@ -43,7 +42,7 @@ describe('createSelectedFieldsSchema', () => {
 			});
 	});
 
-	test('should have a good second element (Wildcard)', () => {
+	test('should have a good second element - Wildcard', () => {
 		const selectedFieldsSchema = createSelectedFieldsSchema(baseSchema);
 		const [, secondElement] = selectedFieldsSchema.anyOf;
 		expect(secondElement[Kind]).toBe('Literal');
@@ -51,7 +50,7 @@ describe('createSelectedFieldsSchema', () => {
 		expect(secondElement.type).toBe('string');
 	});
 
-	test('should have a good third element (Array of Keyof Schema or Wildcard)', () => {
+	test('should have a good third element - Array of Keyof Schema', () => {
 		const selectedFieldsSchema = createSelectedFieldsSchema(baseSchema);
 		const [, , thirdElement] = selectedFieldsSchema.anyOf;
 		expect(thirdElement[Kind]).toBe('Array');
@@ -61,21 +60,15 @@ describe('createSelectedFieldsSchema', () => {
 		expect(thirdElement.items).toBeDefined();
 		expect(thirdElement.items[Kind]).toBe('Union');
 		expect(thirdElement.items.anyOf).toBeDefined();
-		expect(thirdElement.items.anyOf).toHaveLength(2);
+		expect(thirdElement.items.anyOf).toHaveLength(4);
 
 
-		expect(thirdElement.items.anyOf[0][Kind]).toBe('Union');
-		expect(thirdElement.items.anyOf[0].anyOf).toBeDefined();
-		expect(thirdElement.items.anyOf[0].anyOf).toHaveLength(Object.keys(baseSchema.properties).length);
+		expect(thirdElement.items.anyOf).toHaveLength(Object.keys(baseSchema.properties).length);
 		for (const key of Object.keys(baseSchema.properties))
-			expect(thirdElement.items.anyOf[0].anyOf).toContainEqual({
+			expect(thirdElement.items.anyOf).toContainEqual({
 				[Kind]: 'Literal',
 				const: key,
 				type: 'string'
 			});
-
-		expect(thirdElement.items.anyOf[1][Kind]).toBe('Literal');
-		expect(thirdElement.items.anyOf[1].const).toBe('*');
-		expect(thirdElement.items.anyOf[1].type).toBe('string');
 	});
 });
