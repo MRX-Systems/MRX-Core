a763feb110bc170e071c0d278fe71439207caf69
ruby <necrelox@proton.me>
2025-07-21 15:02:25 +0200

    refactor(🧹): [Update CrudSchemaOperations to use optional properties] ## Refactoring - Changed properties in `CrudSchemaOperations` interface from required to optional.
    ## Description
    This change allows for more flexible usage of the `CrudSchemaOperations` interface by making all properties optional. This is beneficial for scenarios where not all CRUD operations need to be defined, enhancing the adaptability of the interface.

--- source/modules/elysia/crudSchemaPlugin/types/crudSchemaOperations.ts
+++ source/modules/elysia/crudSchemaPlugin/types/crudSchemaOperations.ts
@@ -1,10 +1,10 @@
 export interface CrudSchemaOperations {
-	count: true;
-	find: true;
-	findOne: true;
-	insert: true;
-	update: true;
-	updateOne: true;
-	delete: true;
-	deleteOne: true;
+	count?: boolean;
+	find?: boolean;
+	findOne?: boolean;
+	insert?: boolean;
+	update?: boolean;
+	updateOne?: boolean;
+	delete?: boolean;
+	deleteOne?: boolean;
 }
