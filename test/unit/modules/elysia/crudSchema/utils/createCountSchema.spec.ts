b2865408f379359be0ae5cd0d623ee0b885d31d3
ruby <necrelox@proton.me>
2025-07-29 14:26:01 +0200

    test(🧪): [Refactor tests for queryOptions in createCountSchema]
    ## Tests
    - Updated test cases for `queryOptions` to ensure correct type, kind, and optional properties.
    - Separated tests for clarity and improved readability.
    - Ensured that `filters` are optional and correctly defined as a union of partial filter schemas.

--- test/unit/modules/elysia/crudSchema/utils/createCountSchema.spec.ts
+++ test/unit/modules/elysia/crudSchema/utils/createCountSchema.spec.ts
@@ -22,16 +22,37 @@ describe('createCountSchema', () => {
 		expect(schema.properties).toHaveProperty('queryOptions');
 	});
 
-	test('should verify queryOptions has correct type, kind and optionalKind', () => {
+	test('should queryOptions has correct type and kind', () => {
 		const schema = createCountSchema(sourceSchema);
 		expect(schema.properties.queryOptions.type).toBe('object');
 		expect(schema.properties.queryOptions[Kind]).toBe('Object');
-		expect(schema.properties.queryOptions[OptionalKind]).toBe('Optional');
 	});
 
-	test('should queryOptions has only filters', () => {
+	test('should queryOptions has correct properties', () => {
 		const schema = createCountSchema(sourceSchema);
 		expect(schema.properties.queryOptions.properties).toHaveProperty('filters');
-		expect(Object.keys(schema.properties.queryOptions.properties).length).toBe(1);
+	});
+
+	test('should filters be optional', () => {
+		const schema = createCountSchema(sourceSchema);
+		expect(schema.properties.queryOptions.properties.filters[OptionalKind]).toBe('Optional');
+	});
+
+	test('should filters be a union of partial filter schemas', () => {
+		const schema = createCountSchema(sourceSchema);
+		const { filters } = schema.properties.queryOptions.properties;
+
+		expect(filters[Kind]).toBe('Union');
+		expect(filters[OptionalKind]).toBe('Optional');
+		expect(filters.anyOf).toHaveLength(2);
+		expect(filters.anyOf[0][Kind]).toBe('Object');
+		expect(filters.anyOf[0].type).toBe('object');
+		expect(filters.anyOf[1][Kind]).toBe('Array');
+		expect(filters.anyOf[1].type).toBe('array');
+	});
+
+	test('should queryOptions be optional', () => {
+		const schema = createCountSchema(sourceSchema);
+		expect(schema.properties.queryOptions[OptionalKind]).toBe('Optional');
 	});
 });
