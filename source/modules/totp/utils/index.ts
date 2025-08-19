5c098b269764a9c351bb802145498467fbf46771
ruby <necrelox@proton.me>
2025-08-19 11:22:08 +0200

    refactor(🧹): [Totp module little clean]

--- source/modules/totp/utils/index.ts
+++ source/modules/totp/utils/index.ts
@@ -1,6 +1,7 @@
 export { base32Decode, base32Encode } from './base32';
 export { createCounterBuffer } from './createCounterBuffer';
-export { generateSecretBytes } from './generateSecretBytes';
-export { generateHmac } from './generateHmac';
 export { dynamicTruncation } from './dynamicTruncation';
+export { generateHmac } from './generateHmac';
+export { generateSecretBytes } from './generateSecretBytes';
+export { timeRemaining } from './timeRemaining';
 
