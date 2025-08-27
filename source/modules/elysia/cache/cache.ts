a54cd86fc4ae459ece2afad723b53ba450db51d8
ruby <necrelox@proton.me>
2025-08-27 10:15:12 +0200

    feat(🚀): [Add caching functionality with cache utility]
    ## Features
    - Introduced a new `cache` utility for managing request caching.
    - Supports customizable TTL, prefix, and store options.
    - Implements a macro for cache control with `isCached` method.
    
    ## Description
    This commit adds a caching mechanism to the Elysia framework. The `cache` function allows users to specify a default time-to-live (TTL), a prefix for cache keys, and a store type (defaulting to an in-memory store). The caching logic retrieves cached responses based on generated cache keys and sets appropriate headers for cache control. The `isCached` macro enables users to define caching behavior for responses, including setting expiration and last modified headers.

---
+++ source/modules/elysia/cache/cache.ts
@@ -0,0 +1,52 @@
+import { Elysia } from 'elysia';
+
+import { MemoryStore } from '#/modules/kvStore/memory/memoryStore';
+import type { CacheOptions } from './types/cacheOptions';
+import { generateCacheKey } from './utils/generateCacheKey';
+
+export const cache = ({
+	defaultTtl = 60,
+	prefix = '',
+	store = ':memory:'
+}: CacheOptions = {}) => new Elysia()
+	.state(
+		'kvStore',
+		store === ':memory:'
+			? new MemoryStore()
+			: store
+	)
+	.state('_cacheKey', '')
+	.onRequest(async ({ request, store, set }) => {
+		const cacheKey = await generateCacheKey(request.clone());
+		const cachedResponse = await store.kvStore.get(`${prefix}${cacheKey}`);
+
+		store._cacheKey = cacheKey;
+
+		if (cachedResponse) {
+			set.headers['Cache-Control'] = `max-age=${defaultTtl}, public`;
+			set.headers['X-Cache'] = 'HIT';
+			set.headers['ETag'] = `"${prefix}${cacheKey}"`;
+			return cachedResponse;
+		}
+		return void 0;
+	})
+	.macro({
+		isCached: (enable: boolean | number) => {
+			const ttl = typeof enable === 'number'
+				? enable
+				: (enable ? defaultTtl : 0);
+			return {
+				async afterHandle({ set, response, store }) {
+					const cacheKey = store._cacheKey;
+					const now = new Date();
+
+					set.headers['Cache-Control'] = `max-age=${ttl}, public`;
+					set.headers['ETag'] = `"${prefix}${cacheKey}"`;
+					set.headers['Last-Modified'] = now.toUTCString();
+					set.headers['Expires'] = new Date(now.getTime() + (ttl * 1000)).toUTCString();
+
+					await store.kvStore.set(`${prefix}${cacheKey}`, response, ttl);
+				}
+			};
+		}
+	});
