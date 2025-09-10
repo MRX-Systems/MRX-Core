4cf27943e3fccada3ff778628494d8acad5ea73b
ruby <necrelox@proton.me>
2025-09-10 14:32:17 +0200

    test(🧪): [add test for CrudSchema and its utilities]

---
+++ test/unit/modules/elysia/crud/utils/create-filters-schema.spec.ts
@@ -0,0 +1,36 @@
+import { Kind } from '@sinclair/typebox';
+import { describe, expect, test } from 'bun:test';
+import { t } from 'elysia';
+
+import { createFiltersSchema } from '#/modules/elysia/crud/utils/create-filters-schema';
+
+const sourceSchema = t.Object({
+	id: t.Number(),
+	name: t.String(),
+	age: t.Number()
+});
+
+describe('createFiltersSchema', () => {
+	test('should create a schema with a good type and kind', () => {
+		const schema = createFiltersSchema(sourceSchema);
+		expect(schema.type).toBe('object');
+		expect(schema[Kind]).toBe('Object');
+	});
+
+	test('should have the correct properties', () => {
+		const schema = createFiltersSchema(sourceSchema);
+		expect(schema.properties).toHaveProperty('$q');
+		for (const key of Object.keys(sourceSchema.properties))
+			expect(schema.properties).toHaveProperty(key);
+	});
+
+	test('should verify properties are required', () => {
+		const schema = createFiltersSchema(sourceSchema);
+		expect(schema.required).toEqual(['$q', ...Object.keys(sourceSchema.properties)]);
+	});
+
+	test('should have a minimum of one property', () => {
+		const schema = createFiltersSchema(sourceSchema);
+		expect(schema.minProperties).toBe(1);
+	});
+});
---
+++ test/unit/modules/elysia/crud/utils/create-id-param-schema.spec.ts
@@ -0,0 +1,51 @@
+import { Kind } from '@sinclair/typebox';
+import { describe, expect, test } from 'bun:test';
+
+import { createIdParamSchema } from '#/modules/elysia/crud/utils/create-id-param-schema';
+
+describe('createIdParamSchema', () => {
+	test('should create a schema with a good type and kind', () => {
+		const schema = createIdParamSchema();
+		expect(schema.type).toBe('object');
+		expect(schema[Kind]).toBe('Object');
+	});
+
+	test('should have the correct properties', () => {
+		const schema = createIdParamSchema();
+		expect(schema.properties).toHaveProperty('id');
+	});
+
+	test('should id has a good type and kind', () => {
+		const schema = createIdParamSchema();
+		expect(schema.properties.id[Kind]).toBe('Union');
+	});
+
+	test('should required id', () => {
+		const schema = createIdParamSchema();
+		expect(schema.required).toContain('id');
+	});
+
+	test('should id be a union with exactly 2 types', () => {
+		const schema = createIdParamSchema();
+		expect(schema.properties.id.anyOf).toHaveLength(2);
+	});
+
+	test('should id first union type be a string with uuid format', () => {
+		const schema = createIdParamSchema();
+		const [stringType] = schema.properties.id.anyOf;
+
+		expect(stringType[Kind]).toBe('String');
+		expect(stringType.type).toBe('string');
+		expect(stringType.format).toBe('uuid');
+	});
+
+	test('should id second union type be a number with constraints', () => {
+		const schema = createIdParamSchema();
+		const [, numberType] = schema.properties.id.anyOf;
+
+		expect(numberType[Kind]).toBe('Number');
+		expect(numberType.type).toBe('number');
+		expect(numberType.minimum).toBe(1);
+		expect(numberType.maximum).toBe(Number.MAX_SAFE_INTEGER);
+	});
+});
