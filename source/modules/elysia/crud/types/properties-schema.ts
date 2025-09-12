39635ee52ddd0afbaa2768197c67a1c46e7b3943
ruby <necrelox@proton.me>
2025-09-12 09:27:00 +0200

    types(🌊): [Refactor PropertiesSchema]

--- source/modules/elysia/crud/types/properties-schema.ts
+++ source/modules/elysia/crud/types/properties-schema.ts
@@ -1,10 +1,11 @@
 import type { Static, TObject, TUnion } from '@sinclair/typebox/type';
-
 import type { AdaptiveWhereClauseSchema } from './adaptive-where-clause-schema';
 
+import type { TFlatten } from '#/shared/types/tflatten';
+
 export type PropertiesSchema<TSourceSchema extends TObject> = TObject<{
-	[K in keyof Static<TSourceSchema>]: TUnion<[
+	[K in keyof Static<TSourceSchema>]: TFlatten<TUnion<[
 		AdaptiveWhereClauseSchema<TSourceSchema['properties'][K]>,
 		TSourceSchema['properties'][K]
-	]>
+	]>>
 }>;
