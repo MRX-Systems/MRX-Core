a5495345016f3c256502e45dbd98ad62bae4b4fe
ruby <necrelox@proton.me>
2025-09-15 10:30:18 +0200

    refactor(🧹): [Refactor type exports in index files] - Updated type exports in `source/shared/types/index.ts` to include `TFlatten`. - Corrected export statement in `source/shared/utils/index.ts` for `flatten`.

--- source/shared/utils/index.ts
+++ source/shared/utils/index.ts
@@ -1,5 +1,5 @@
 export { validateEnv } from './env';
-export { Flatten } from './flatten';
+export { flatten } from './flatten';
 export { isDateString } from './is-date-string';
 export { makeStreamAsyncIterable } from './stream';
 
