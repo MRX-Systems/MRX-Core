a65bd49075c68c65ea3eb928853d254ddef54477
ruby <necrelox@proton.me>
2025-12-02 17:12:48 +0100

    refactor(🧹): [Remove CRUD, Type Definitions and Utility Functions...] This commit removes several TypeScript type definitions and utility functions related to CRUD operations. The following files were deleted:
    - CRUD schema operation types
    - CRUD schema options types
    - Delete schema types
    - Filter schema types
    - Find schema types
    - ID parameter schema types
    - Insert schema types
    - Order schema types
    - Properties schema types
    - Q schema types
    - Response 200 schema types
    - Selected field schema types
    - Update one schema types
    - Update schema types
    - Utility functions for creating adaptive where clause schema, count response schema, count schema, delete schema, filters schema, find schema, ID param schema, insert schema, order schema, properties schema, Q schema, response 200 schema, selected fields schema, update one schema, and update schema.
    
    This cleanup helps streamline the codebase by removing unused or redundant code, improving maintainability and readability.

--- source/modules/elysia/crud/types/selected-field-schema.ts
+++
@@ -1,7 +0,0 @@
-import type { TArray, TKeyOf, TLiteral, TObject, TUnion } from '@sinclair/typebox/type';
-
-export type SelectedFieldsSchema<TSourceSchema extends TObject> = TUnion<[
-	TKeyOf<TSourceSchema>,
-	TLiteral<'*'>,
-	TArray<TKeyOf<TSourceSchema>>
-]>;
--- source/modules/elysia/crud/types/update-one-schema.ts
+++
@@ -1,5 +0,0 @@
-import type { TObject, TPartial } from '@sinclair/typebox/type';
-
-export type UpdateOneSchema<TSourceSchema extends TObject> = TObject<{
-	data: TPartial<TSourceSchema>
-}>;
