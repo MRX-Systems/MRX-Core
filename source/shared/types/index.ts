7c018ab9bc21a0b32aff7802e5368f59e36dc258
ruby <necrelox@proton.me>
2025-12-02 17:20:33 +0100

    refactor(🧹): [Remove unused flatten type and utility functions] ## Refactoring - Deleted `TFlatten` type definition from `index.ts`. - Removed `flatten` utility function from `flatten.ts`. ## Description This commit cleans up the codebase by removing the `TFlatten` type and its associated utility function, which were not being utilized in the project. This helps in reducing clutter and improving maintainability.

--- source/shared/types/index.ts
+++ source/shared/types/index.ts
@@ -1,5 +1,4 @@
 export type { AddPrefixToAllKeys } from './add-prefix-to-all-key';
 export type { RenameKey } from './rename-key';
 export type { StreamWithAsyncIterable } from './stream-with-async-iterable';
-export type { TFlatten } from './tflatten';
 
